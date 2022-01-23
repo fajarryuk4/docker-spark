eed40dd8f9533d69cb8af99b5fb8e82b356c3758
fajarryuk4 <captainfreedom37@gmail.com>
2022-01-23 13:01:59 +0700

    add conda and mamba library to spark

---
+++ base/require/install-lang.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+apk update
+
+apk --no-cache add cmake make musl-dev gcc gettext-dev libintl && \
+    wget https://gitlab.com/rilian-la-te/musl-locales/-/archive/master/musl-locales-master.zip && \
+    unzip musl-locales-master.zip && \
+    cd musl-locales-master && \
+    cmake -DLOCALE_PROFILE=OFF -D CMAKE_INSTALL_PREFIX:PATH=/usr . && \
+    make && make install && \
+    cd .. && rm -rf musl-locales-master musl-locales-master.zip
+
+apk del --purge cmake make musl-dev gcc gettext-dev && \
+    locale -a
